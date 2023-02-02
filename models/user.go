bb98d6d432c7650c558f687b480a164e47335156
Ajinkya <ajinkya001@e.ntu.edu.sg>
2023-02-02 23:33:21 +0800

    created user model

--- models/user.go
+++ models/user.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name        string             `bson:"name" json:"name"`
+	Dob         time.Time          `bson:"dob" json:"dob"`
+	Address     string             `bson:"address" json:"address"`
+	Description string             `bson:"description" json:"description"`
+	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
+}
