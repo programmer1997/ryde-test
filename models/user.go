8e007729a89d07d29e23a0f7315944954aada690
Ajinkya <ajinkya001@e.ntu.edu.sg>
2023-02-03 22:32:57 +0800

    made changes to model

--- models/user.go
+++ models/user.go
@@ -8,8 +8,8 @@ import (
 type User struct {
 	Id          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name        string             `bson:"name" json:"name"`
-	Dob         time.Time          `bson:"dob" json:"dob"`
-	Address     string             `bson:"address" json:"address"`
-	Description string             `bson:"description" json:"description"`
+	Dob         time.Time          `bson:"dob,omitempty" json:"dob"`
+	Address     string             `bson:"address,omitempty" json:"address"`
+	Description string             `bson:"description,omitempty" json:"description"`
 	CreatedAt   time.Time          `bson:"createdAt" json:"createdAt"`
 }
